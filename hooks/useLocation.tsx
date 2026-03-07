f073670e3aa533dc5699943daa0dfb75a8316bc5
rednadp <rednadp@gmail.com>
2026-03-07 18:04:53 +0100

    Fixed location request permmission

--- hooks/useLocation.tsx
+++ hooks/useLocation.tsx
@@ -4,19 +4,32 @@ import { useEffect, useState } from "react";
 export function useLocation() {
     const [location, setLocation] = useState<Location.LocationObject | null>(null)
     const [errorMsg, setErrorMsg] = useState<string | null>(null)
-
+    console.log("HOOOOLA")
     useEffect(() => {
         async function getPermissions() {
-            let {status} = await Location.requestBackgroundPermissionsAsync()
+            try {
+                let {status} = await Location.requestForegroundPermissionsAsync()
 
             if (status !== 'granted') {
-                setErrorMsg("Location permission denied, we cannot located you")
+                setErrorMsg("Foregound Location permission denied, we cannot located you")
                 return
             }
 
+            let backgoundStatus = await Location.requestBackgroundPermissionsAsync()
+
+            if (backgoundStatus.status !== 'granted') {
+                setErrorMsg("Backgound location denied")
+            }
+
             let location = await Location.getCurrentPositionAsync({})
+            console.log(location)
 
             setLocation(location)
+            } catch (error) {
+                setErrorMsg("Error")
+                console.log(error)
+            }
+            
 
             const subscriber = await Location.watchPositionAsync(
                 {
