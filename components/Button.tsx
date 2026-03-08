23f08f6ca2906d4ca6e5bbc957470a691fba1c1a
rednadp <rednadp@gmail.com>
2026-03-08 14:27:46 +0100

    Contiunued with the map selector layout

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,14 +8,37 @@ export function Button({label}: Props) {
 
 
     return (
-        <View>
-            <Pressable onPress={() => {alert("Pulsado")}}>
-                <Text>Touch me</Text>
+        <View style={style.contatiner}>
+            <Pressable style={style.button} onPress={() => {alert("Pulsado")}}>
+                <Text style={style.text}>{label}</Text>
             </Pressable>
         </View>
     )
 }
 
 const style = StyleSheet.create({
-
+    contatiner : {
+        width: 320,
+        height: 68,
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 3,
+        borderColor: "#fff",
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: '#25292e'
+    },
+    button: {
+        borderRadius: 10,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    text: {
+        fontSize: 16,
+        color: '#ff5b5b'
+    }
 })
